b370bf1406e9d873324db02a384bdcff58ad7d0f
Jacob Leamer <jleamer@tulane.edu>
2020-01-15 10:51:57 -0600

    implemented a version to handle stokes parameter input in addition to intensity input and merged into one file before tidying up code

--- err_corr2.py
+++ err_corr2.py
@@ -27,6 +27,82 @@ def bloch_from_dataframe(df, axes):
 
     bloch.make_sphere()
 
+def int_corr(filename):
+    """
+    Function to minimize the coherency matrix from the provided data
+    :param data: pd.DataFrame object
+    :return:     the list of minimized coherency matrices
+    """
+    wb = pd.read_excel(filename, sheet_name=None)
+    data = wb['measured']
+    size = data.values.shape[0]
+    min_J = []
+    columns = ['theta', 'Jxx', 'Jyy', 'beta', 'gamma', 'trace']
+
+    #Slice the intensities out of the data file and normalize them
+    I = np.array([data.values[i][1:5] for i in range(size)])
+    for i in I:
+        i /= (i[0] + i[1])
+
+    for i in range(size):
+        J = cp.Variable((2, 2), PSD=True)
+        cost = cp.abs(I[i][0] -cp.trace((l1.conj().T).dot(l1) @ J)) ** 2 + \
+               cp.abs(I[i][1] -cp.trace((l2.conj().T).dot(l2) @ J)) ** 2 + \
+               cp.abs(I[i][2] -cp.trace((l3.conj().T).dot(l3) @ J)) ** 2 + \
+               cp.abs(I[i][3] -cp.trace((l4.conj().T).dot(l4) @ J)) ** 2
+
+        constraint = [cp.trace(J) <= 1]
+        prob = cp.Problem(cp.Minimize(cost), constraint)
+        prob.solve()
+        
+        min_J.append([data.values[i][0], J.value[0][0], J.value[1][1], np.real(J.value[0][1]), np.imag(J.value[0][1]), np.trace(J.value.dot(J.value))])
+
+    min_J = np.array(min_J)
+    temp = pd.DataFrame(min_J, columns=columns)
+    wb['adjusted'] = temp
+
+    with pd.ExcelWriter(filename) as writer:
+        for key in wb:
+            wb[key].to_excel(writer, sheet_name=key, index=False)
+
+def stokes_corr(filename):
+    """
+    Function to minimize the coherency matrix from the provided data
+    :param data: pd.DataFrame object
+    :return:     the list of minimized coherency matrices
+    """
+    wb = pd.read_excel(filename, sheet_name=None)
+    data = wb['measured']
+    size = data.values.shape[0]
+    min_J = []
+    columns = ['theta', 'Jxx', 'Jyy', 'beta', 'gamma', 'trace']
+
+    #Slice the Stokes parameters out of the data file and normalize them
+    S = np.array([data.values[i][1:5] for i in range(size)])
+    for s in S:
+        s /= s[0] 
+
+    for i in range(size):
+        J = cp.Variable((2, 2), PSD=True)
+        cost = (S[i][0] - (J[0][0] + J[1][1])) ** 2 + \
+               (S[i][1] - (J[0][0] - J[1][1])) ** 2 + \
+               (S[i][2] - 2 * np.real(J[0][1])) ** 2 + \
+               (S[i][3] - 2 * np.imag(J[0][1])) ** 2
+
+        constraint = [cp.trace(J) <= 1]
+        prob = cp.Problem(cp.Minimize(cost), constraint)
+        prob.solve()
+        
+        min_J.append([data.values[i][0], J.value[0][0], J.value[1][1], np.real(J.value[0][1]), np.imag(J.value[0][1]), np.trace(J.value.dot(J.value))])
+
+    min_J = np.array(min_J)
+    temp = pd.DataFrame(min_J, columns=columns)
+    wb['adjusted'] = temp
+
+    with pd.ExcelWriter(filename) as writer:
+        for key in wb:
+            wb[key].to_excel(writer, sheet_name=key, index=False)
+
 if __name__=="__main__":
 
     '''
@@ -52,50 +128,17 @@ if __name__=="__main__":
     l = [l1, l2, l3, l4]
 
     #Read in data from excel sheet and normalize intensities
-    filename = 'HWP_hand_high.xlsx'
-    data = pd.read_excel(filename, sheet_name='measured')
-    I = np.array([data.values[i][1:5] for i in range(data.values.shape[0])])
-    for i in I:
-        i /= (i[0] + i[1])
+    filename = 'HWP_hand_low.xlsx'
+    int_corr(filename)
 
-    #Set up variables for optimization
-    #   Each element of J is the coherency matrix for different polarization angles from the file
-    J = [cp.Variable((2, 2), PSD=True) for i in range(data.values.shape[0])]
-
-    #Apply constraints of positivity and hermitian to J
-    cost = [cp.abs(I[i][0] -cp.trace((l1.conj().T).dot(l1) @ J[i])) ** 2 + \
-           cp.abs(I[i][1] -cp.trace((l2.conj().T).dot(l2) @ J[i])) ** 2 + \
-           cp.abs(I[i][2] -cp.trace((l3.conj().T).dot(l3) @ J[i])) ** 2 + \
-           cp.abs(I[i][3] -cp.trace((l4.conj().T).dot(l4) @ J[i])) ** 2 \
-            for i in range(data.values.shape[0])]
-
-    prob = [cp.Problem(cp.Minimize(cost[i])) for i in range(data.values.shape[0])]
-
-    for i in range(data.values.shape[0]):
-        prob[i].solve()
-        #print(J[i].value)
-
-    #Write the coherency matrix entries to the excel spreadsheet
-	#First construct the dataframe
-    columns = ['theta', 'Jxx', 'Jyy', 'beta', 'gamma', 'trace']
-
-    temp = []
-    for i in range(data.values.shape[0]):
-	    temp.append([data.values[i][0], J[i].value[0][0], J[i].value[1][1], np.real(J[i].value[0][1]),\
-                     np.imag(J[i].value[0][1]), np.trace(J[i].value.dot(J[i].value))])
-    temp = np.array(temp)
-    results = pd.DataFrame(temp, columns=columns, dtype=float)
-
-    wb = pd.read_excel(filename, sheet_name=None)
-    wb['Adjusted2'] = results
-    with pd.ExcelWriter(filename) as writer:
-    	for key in wb:
-    		wb[key].to_excel(writer, sheet_name=key, index=False)
+    filename2 = 'HWP_polarimeter_low.xlsx'
+    stokes_corr(filename)
+    
 
     ####################################################################################
 
     #Plot on Bloch sphere
-    fig = plt.figure(figsize=plt.figaspect(0.3))
+    fig = plt.figure(1, figsize=plt.figaspect(0.3))
     fig_title = "Correction for " + filename
     fig.suptitle(fig_title)
 
@@ -109,7 +152,7 @@ if __name__=="__main__":
     ax = fig.add_subplot(122, projection='3d', azim=0, elev=20)
     ax.set_title("Adjusted", loc='left')
 
-    bloch_from_dataframe(pd.read_excel(filename, sheet_name='Adjusted2'), ax)
+    bloch_from_dataframe(pd.read_excel(filename, sheet_name='adjusted'), ax)
 
     #Set some plotting configs
     norm = mpl.colors.Normalize(vmin=0, vmax=90)
@@ -119,4 +162,19 @@ if __name__=="__main__":
     fig.colorbar(sm, ticks=np.linspace(0, 90, 10), cax=cbaxes)
              #boundaries=np.arange(-0.05,2.1,.1))
 
+    fig2 = plt.figure(2, figsize=plt.figaspect(0.3))
+    fig2_title = "Correction for " + filename2
+    fig2.suptitle(fig2_title)
+
+    ax2 = fig2.add_subplot(121, projection='3d', azim=0, elev=20)
+    ax2.set_title("Calculated from data", loc='left')
+
+    bloch_from_dataframe(pd.read_excel(filename2, sheet_name='calculated'), ax2)
+    
+    ax2 = fig2.add_subplot(122, projection='3d', azim=0, elev=20)
+    ax2.set_title("Adjusted", loc='left')
+
+    bloch_from_dataframe(pd.read_excel(filename2, sheet_name='adjusted'), ax2)
+    cbaxes = fig2.add_axes([0.9, 0.1, 0.03, 0.8])
+    fig2.colorbar(sm, ticks=np.linspace(0, 90, 10), cax=cbaxes)
     plt.show()
